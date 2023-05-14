e00bf2bb500381309d8465e62313395ee51e769c
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-14 08:12:45 +0600

    login access by jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config();
@@ -34,6 +35,20 @@ async function run() {
     const servicesCollection = client.db('carDoctor').collection('services')
     const bookingCollection = client.db('carDoctor').collection('bookings')
 
+    // jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.JWT_TOKEN, {
+        expiresIn: '1h'
+      })
+      res.send({ token })
+      console.log({ token });
+
+    })
+
+
+    // services routes
     app.get('/services', async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
